ee8509688a208fb7466b4d42b9dbbd5b337f2b12
Anastasia <nstanastasia25@yandex.ru>
2022-05-25 21:33:55 +0300

    added tasks home work 8

---
+++ seminar8task1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Заполните спирально массив 4 на 4
+namespace matrix
+{
+    class Program {
+        static void Main() {
+            Console.Write(" n = ");
+            int N = int.Parse(Console.ReadLine());//границы снизу и сверху, границы слева и спава
+           
+ 
+            int[,] NN = new int[N, N];            
+            int row = 0, col = 0, dx = 0, dy = 1, dirChanges = 0, gran = N;
+            int l =  NN.Length;
+            for(int i = 0; i < l; i++) {
+                NN[row, col] = i + 1;
+ 
+                if(--gran == 0) {
+                    gran = N * (dirChanges % 2) + N * ((dirChanges + 1) % 2) - (dirChanges / 2 - 1) - 2;
+                    int temp = dy;
+                    dy = -dx;
+                    dx = temp;
+                    dirChanges++;
+                }
+ 
+                col += dx;
+                row += dy;
+            }
+                        
+            N = NN.GetLength(1);
+            for(int k = 0; k < l; k++)
+                Console.Write("{0}{1,3}", k % N == 0 ? "\n" : "", NN[k / N, k % N]);
+ 
+ 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ seminar8task2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[][] jagged = new int[3][];
+jagged[0] = new int[4];
+jagged[1] = new int[3];
+jagged[2] = new int[5];
+
+// Заполняем массив случайными числами
+Random random = new Random();
+for (int i = 0; i < jagged.Length; i++)
+{
+    for (int j = 0; j < jagged[i].Length; j++)
+        jagged[i][j] = random.Next(-10, 10);
+}
+
+// Исходное содержимое массива
+Console.WriteLine("Исходное содержимое массива");
+foreach (int[] item in jagged)
+{
+    foreach (int element in item)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+// Сортировка массива
+foreach (int[] item in jagged)
+{
+    foreach (int element in item)
+        Array.Sort(item);
+}
+
+// Отсортированное содержимое массива
+Console.WriteLine("\nОтсортированное содержимое массива");
+foreach (int[] item in jagged)
+{
+    foreach (int element in item)
+        Console.Write(element + " ");
+    Console.WriteLine();
+}
+
+Console.ReadKey();
